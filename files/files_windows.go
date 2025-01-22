cf76ed63788f58cf8095444f82558e14c0ccafb5
nullpointerException <nullpointerException@noreply.gitcode.com>
2025-01-22 11:10:19 +0800

    fix：修复部分bug

--- files/files_windows.go
+++ files/files_windows.go
@@ -10,24 +10,6 @@ import (
 	"time"
 )
 
-func GetStorageFile() (*os.File, error) {
-	var file *os.File
-	var err error
-	appData := os.Getenv("APPDATA")
-
-	filePath := filepath.Join(appData, "Cursor", "User", "globalStorage", "storage.json")
-
-	file, err = os.OpenFile(
-		filePath,
-		os.O_RDWR|os.O_CREATE,
-		0666,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
-}
-
 func BackupFile(filePath string) error {
 	if _, err := os.Stat(filePath); err == nil {
 		timestamp := time.Now().Format("20060102_150405")
